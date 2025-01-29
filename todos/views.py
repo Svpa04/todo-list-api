1578fe73c21bc11a65d734348f65c0863c3dc83d
Svpa04 <sofi04122001@gmail.com>
2025-01-29 16:56:21 -0400

    todo detail done

--- todos/views.py
+++ todos/views.py
@@ -7,3 +7,7 @@ from .serializers import TodoSerializer
 class ListTodo(generics.ListAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+
+class DetailTodo(generics.RetrieveAPIView):
+    queryset = Todo.objects.all() 
+    serializer_class = TodoSerializer #uno solo
